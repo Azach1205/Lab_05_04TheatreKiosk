2a93d9a89480add8ec39df9a87f3114355d387a2
Zachary Stewart <stewarzy@mail.uc.edu>
2024-09-26 14:39:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        //declare variables
+        int age = 0;
+        int DRINKING_AGE = 21;
+
+        System.out.print("Enter your age: ");
+
+        if(in.hasNextInt())
+        {
+            age = in.nextInt();
+            in.nextLine();
+        }
+
+        if(age >= DRINKING_AGE)
+        {
+            System.out.println("You need to get a wrist band. Drink resposibly.");
+        }
+    }
+}
